1135027f3dff537b93f9f150cf1ee8a846267b32
AntonyCorbett <antony@corbetts.org.uk>
2022-01-05 17:09:18 +0000

    bumped version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,8 +2,8 @@
 
 [assembly: AssemblyCompany("Antony Corbett")]
 [assembly: AssemblyProduct("JWLMerge")]
-[assembly: AssemblyCopyright("Copyright © 2019, 2021")]
+[assembly: AssemblyCopyright("Copyright © 2019, 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.7")]
+[assembly: AssemblyVersion("2.0.0.8")]
